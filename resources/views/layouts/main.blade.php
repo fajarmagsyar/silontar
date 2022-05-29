b8e6a3d0e7fade8833017d03385a05b6251931e7
kokomahbebas <kokoakuncoc05@gmail.com>
2022-05-29 22:29:54 +0800

    serah

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,191 +3,131 @@
 
 <head>
     <meta charset="utf-8">
-    <title>PAINTER - Painting Company Website Template</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
 
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <title>SILONTAR</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Favicons -->
+    <link href="/assets/img/Logo.png" rel="icon">
+    <link href="/assets/img/logo1.png" rel="apple-touch-icon">
 
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
-    <link href="/user/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="/user/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ======= icon boostrap ============== -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-    <!-- Template Stylesheet -->
-    <link href="/user/css/style.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="/assets/css/style.css" rel="stylesheet">
+
+
 </head>
 
 <body>
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-primary d-none d-lg-block">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
-                    <div class="d-inline-flex align-items-center">
-                        <a class="text-dark py-2 pe-3 border-end border-white" href=""><i class="bi bi-telephone text-secondary me-2"></i>+012 345 6789</a>
-                        <a class="text-dark py-2 px-3" href=""><i class="bi bi-envelope text-secondary me-2"></i>info@example.com</a>
-                    </div>
-                </div>
-                <div class="col-md-6 text-center text-lg-end">
-                    <div class="d-inline-flex align-items-center">
-                        <a class="text-body py-2 px-3 border-end border-white" href="">
-                            <i class="fab fa-facebook-f text-secondary"></i>
-                        </a>
-                        <a class="text-body py-2 px-3 border-end border-white" href="">
-                            <i class="fab fa-twitter text-secondary"></i>
-                        </a>
-                        <a class="text-body py-2 px-3 border-end border-white" href="">
-                            <i class="fab fa-linkedin-in text-secondary"></i>
-                        </a>
-                        <a class="text-body py-2 px-3 border-end border-white" href="">
-                            <i class="fab fa-instagram text-secondary"></i>
-                        </a>
-                        <a class="text-body py-2 ps-3" href="">
-                            <i class="fab fa-youtube text-secondary"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center justify-content-lg-between">
+
+            <h1 class="logo me-auto me-lg-0"><a href="index.php">SILONTAR<span>.</span></a></h1>
+
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">BERANDA</a></li>
+                    <li><a class="nav-link scrollto" href="#services">PANDUAN UMUM</a></li>
+                    <li><a class="nav-link scrollto" href="#">TENTANG</a></li>
+                    <li><a class="nav-link scrollto" href="#">KONTAK</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
         </div>
-    </div>
-    <!-- Topbar End -->
-
-
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm px-5 py-3 py-lg-0">
-        <a href="index.html" class="navbar-brand p-0">
-            <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-paint-roller text-secondary me-3"></i>Painter</h1>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto py-0 pe-4 border-end border-5 border-primary">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="service.html" class="nav-item nav-link">Service</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu m-0">
-                        <a href="blog.html" class="dropdown-item">Blog Grid</a>
-                        <a href="detail.html" class="dropdown-item">Blog Detail</a>
-                        <a href="team.html" class="dropdown-item">The Team</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                    </div>
-                </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
-            </div>
-            <div class="d-none d-lg-flex align-items-center ps-4">
-                <i class="fa fa-2x fa-mobile-alt text-secondary me-3"></i>
-                <div>
-                    <h5 class="text-primary mb-1"><small>Call Now</small></h5>
-                    <h6 class="text-light m-0">+012 345 6789</h6>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- Navbar End -->
-
-
+    </header><!-- End Header -->
     @yield('konten')
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark bg-footer text-light py-5">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-primary">Get In Touch</h4>
-                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
-                    <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor</p>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
-                    <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-primary">Our Services</h4>
-                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Regular Painting</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Wall Painting</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Floor Coating</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Graffiti Removal</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Mildew Removal</a>
-                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Window Washing</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-primary">Quick Links</h4>
-                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-primary">Newsletter</h4>
-                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
-                    <form action="">
-                        <div class="input-group">
-                            <input type="text" class="form-control p-3 border-0" placeholder="Your Email">
-                            <button class="btn btn-primary">Sign Up</button>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-info">
+                            <h3>SILONTAR<span>.</span></h3>
+                            <p>
+                                BALAI JALAN X NUSA TENGGARA TIMUR<br>
+                                <br><br>
+                                <strong>Phone:</strong> +62<br>
+                                <strong>Email:</strong> balaijalanxntt@gmail.com<br>
+                            </p>
+                            <div class="social-links mt-4">
+                                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            </div>
                         </div>
-                    </form>
-                    <h6 class="text-primary mt-4 mb-3">Follow Us</h6>
-                    <div class="d-flex">
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle" href="#"><i class="fab fa-instagram"></i></a>
                     </div>
+
+
+                    <div class="col-lg-4 col-md-6 footer-links">
+                        <h4>Link</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="/">BERANDA</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="/">PANDUAN UMUM</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="daftar.php">DAFTAR</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">LOGIN</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">TENTANG</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Alamat</h4>
+                        <p>Tanah Merah, Kec. Kupang Tengah, Kabupaten Kupang, Nusa Tenggara Tim.</p>
+
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.845100346634!2d123.72670511438471!3d-10.111762511673845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2c5681962dcf8cb7%3A0xe35b0e878df8a43d!2sBalai%20Pelaksanaan%20Jalan%20Nasional%20NTT!5e0!3m2!1sid!2sid!4v1650532982237!5m2!1sid!2sid" width="100%" height="270" style="border:0;"></iframe>
+
+                    </div>
+
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container-fluid bg-primary text-dark py-4">
+
         <div class="container">
-            <div class="row g-0">
-                <div class="col-md-6 text-center text-md-start">
-                    <p class="mb-md-0">Copyright &copy; <a class="text-dark fw-bold" href="#">Your Site Name</a>. All Rights Reserved.</p>
-                </div>
-                <div class="col-md-6 text-center text-md-end">
-                    <p class="mb-0">Designed by <a class="text-dark fw-bold" href="https://htmlcodex.com">HTML Codex</a></p>
-                    <p><br>Distributed By: <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a></p>
-                </div>
+            <div class="copyright">
+                &copy; Copyright <strong><span>SILONTAR</span></strong>. BALAI JALAN X NUSA TENGGARA TIMUR
+            </div>
+            <div class="credits">
+                Designed by: ESC17
             </div>
         </div>
-    </div>
-    <!-- Footer End -->
+    </footer><!-- End Footer -->
 
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-secondary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="/assets/vendor/purecounter/purecounter.js"></script>
+    <script src="/assets/vendor/aos/aos.js"></script>
+    <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="/assets/vendor/php-email-form/validate.js"></script>
 
+    <!-- Template Main JS File -->
+    <script src="/assets/js/main.js"></script>
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/user/lib/easing/easing.min.js"></script>
-    <script src="/user/lib/waypoints/waypoints.min.js"></script>
-    <script src="/user/lib/owlcarousel/owl.carousel.min.js"></script>
-
-    <!-- Template Javascript -->
-    <script src="/user/js/main.js"></script>
 </body>
 
 </html>
