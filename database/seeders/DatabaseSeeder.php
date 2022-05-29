3d4cf33f2589d25d96aba05f6d5cfba49b5b77e9
Fajar Magsyar <84609241+fajarmagsyar@users.noreply.github.com>
2022-05-29 12:25:08 +0800

    Tambah migrasi, modal, dan seeder untuk user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'nik' => '1234567890123001',
+            'nama' => 'Ayam User',
+            'email' => 'ayam@gmail.com',
+            'password' => Hash::make('admin1234'),
+            'jk' => 'Laki-laki',
+            'perusahaan' => 'PT. Tiga Roda',
+            'alamat_perorang' => 'Jln. Bro',
+            'alamat_perusahaan' => 'Jln. Sis',
+            'role' => 'user',
+        ]);
+        User::create([
+            'nik' => '1234567890123002',
+            'nama' => 'Kucing Admin',
+            'email' => 'kucing@gmail.com',
+            'password' => Hash::make('admin1234'),
+            'jk' => 'Perempuan',
+            'perusahaan' => 'PT. Tiga Roda',
+            'alamat_perorang' => 'Jln. Bro',
+            'alamat_perusahaan' => 'Jln. Sis',
+            'role' => 'admin',
+        ]);
     }
 }
