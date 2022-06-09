051eb36cbfdb2e9142c06db60571d3c3670ac920
Fajar Magsyar <84609241+fajarmagsyar@users.noreply.github.com>
2022-06-09 22:55:26 +0800

    berubah

--- resources/views/pengajuan-list.blade.php
+++ resources/views/pengajuan-list.blade.php
@@ -157,7 +157,9 @@
                                             </td>
                                             <td class="align-middle text-sm">
                                                 <div class="col">
-                                                    <button class="btn btn-info text-white"><i class="bi bi-search"></i>
+                                                    <button class="btn btn-info text-white" class="btn btn-primary"
+                                                        data-bs-toggle="modal" data-bs-target="#exampleModal"><i
+                                                            class="bi bi-search"></i>
                                                         Cek
                                                         Status</button>
                                                 </div>
@@ -172,10 +174,32 @@
                                                 </div>
                                             </td>
                                         </tr>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        ...
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     @endforeach
 
                                 </tbody>
                             </table>
+                            <center>{{ $pengajuan->links() }}</center>
                         </div>
                     </div>
                 </div>
